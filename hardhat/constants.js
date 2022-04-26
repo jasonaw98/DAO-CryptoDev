4578b7a2b52a825ccf7b67d94cb0c09d9b62790d
Jasonaw <jason.aw@grassvalley.com>
2022-04-26 19:01:52 +0800

    first commit

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x748e7Cb7aC93D1ACF275C4E6E9F9d59277a0BE39";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
